446e5f81d5669957266320a41700d2d15e476a77
Giovanna Cabral <127431201+eugiovannacabral@users.noreply.github.com>
2025-07-14 15:29:22 -0300

    Create LIST - Pilha.cpp
    Faça um programa que leia 4 valores inteiros e os insira em uma pilha. Depois o programa deve ir removendo os elementos da pilha e mostrando-os na tela.

---
+++ Revisão sobre Lista/List - Pilha.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <list>
+
+using namespace std;
+
+int main()
+{
+    list <int> lista;
+    list <int> ::iterator p;
+    int i;
+    int x;
+    
+    for(i = 0; i < 4; i++) {
+        cin >> x;
+        lista.push_front(x);
+    }
+    
+    // for(p = lista.begin(); p != lista.end(); p++)
+    // {
+    //     cout << *p << " ";
+    // }
+    
+    while (!lista.empty())  //lista.empty  di enquanto ela não for vazia // é só usado para limpar
+    {
+        cout << *lista.begin() << " ";
+        lista.pop_front(); //remove p elemento na lista
+    }
+    
+    return 0;
+}
